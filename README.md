f84c264fb5dc5995cf5f02fa597798a098e6902f
Alex Aiezza <alex.aiezza@daveramsey.com>
2020-04-18 18:17:41 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
-# create-release
-GitHub Action to create a release from a specified branch
+# Create Release for Github Actions
+
+Create a release from a specified branch
+
+## Inputs
+
+### `release_branch`
+
+Branch to tag. Default `master`.
+
+### `name`
+
+The title of the release. Default `release: version ${NEXT_RELEASE}`.
+
+### `message`
+
+The message of the release. Default generate conventional changelog.
+
+### `draft`
+
+Is a draft ?. Default `false`.
+
+### `prerelease`
+
+Is a pre-release ?. Default `false`.
+
+### `create_release`
+
+Create a new release ?. Default `true`.
+
+### `tag`
+
+Tag to use
+
+
+
+## Output
+
+### `release`
+
+The new release name.
+
+### `upload_url`
+The URL for uploading assets to the release, which could be used by GitHub Actions for additional uses, for example the [`@actions/upload-release-asset`](https://www.github.com/actions/upload-release-asset) GitHub Action
+
+## Example usage
+
+```yaml
+    steps:
+      - uses: actions/checkout@v2.1.0
+
+      - name: Create Release
+        uses: aaiezza/create-release@master
+        id: create_release
+        with:
+          release_branch: releases/0.0.1
+          tag: 0.0.1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+
+     - name: Upload Jar
+       uses: actions/upload-release-asset@v1.0.2
+       env:
+         GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+       with:
+         upload_url: ${{ steps.create_release.outputs.upload_url }}
+         asset_path: target/${{ needs.version.outputs.jar-name }}
+         asset_name: ${{ needs.version.outputs.release-jar-name }}
+         asset_content_type: application/java-archive
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,68 @@
+#!/bin/sh -l
+
+set -u
+
+cd /github/workspace || exit 1
+
+# Env and options
+if [ -z "${GITHUB_TOKEN}" ]
+then
+    echo "The GITHUB_TOKEN environment variable is not defined."
+    exit 1
+fi
+
+BRANCH="${1}"
+NAME="${2}"
+MESSAGE="${3}"
+DRAFT="${4}"
+PRERELEASE="${5}"
+CREATE_RELEASE="${6}"
+TAG="${7}"
+
+# Fetch git tags
+git fetch --depth=1 origin +refs/tags/*:refs/tags/*
+
+LAST_HASH=$(git rev-list --tags --max-count=1)
+echo "Last hash : ${LAST_HASH}"
+
+LAST_RELEASE=$(git describe --tags "${LAST_HASH}")
+echo "Last release : ${LAST_RELEASE}"
+MAJOR_LAST_RELEASE=$(echo "${LAST_RELEASE}" | awk -v l=${#NEXT_RELEASE} '{ string=substr($0, 1, l); print string; }' )
+echo "Last major release : ${MAJOR_LAST_RELEASE}"
+
+if [ "${MAJOR_LAST_RELEASE}" = "${TAG}" ]; then
+    MINOR_LAST_RELEASE=$(echo "${LAST_RELEASE}" | awk -v l=`expr ${#TAG} + 2` '{ string=substr($0, l); print string; }' )
+    NEXT_RELEASE=${MAJOR_LAST_RELEASE}.$((${MINOR_LAST_RELEASE} + 1))
+    echo "Minor release"
+fi
+
+if [ "${NAME}" = "0" ]; then
+	NAME="release: version ${TAG}"
+fi
+
+if [ "${MESSAGE}" = "0" ]; then
+  MESSAGE=$(conventional-changelog)
+fi
+
+echo "Next release : ${TAG}"
+
+echo "${MESSAGE}"
+
+echo "Create release : ${CREATE_RELEASE}"
+
+if [ "${CREATE_RELEASE}" = "true" ] || [ "${CREATE_RELEASE}" = true ]; then
+  JSON_STRING=$( jq -n \
+                    --arg tn "$TAG" \
+                    --arg tc "$BRANCH" \
+                    --arg n "$NAME" \
+                    --arg b "$MESSAGE" \
+                    --argjson d "$DRAFT" \
+                    --argjson p "$PRERELEASE" \
+                    '{tag_name: $tn, target_commitish: $tc, name: $n, body: $b, draft: $d, prerelease: $p}' )
+  echo ${JSON_STRING}
+  OUTPUT=$(curl -s --data "${JSON_STRING}" "https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}")
+  echo ${OUTPUT} | jq
+fi;
+
+echo ::set-output name=release::"${TAG}"
+echo ::set-output name=upload_url::"`echo ${OUTPUT} | jq '.upload_url'`"
